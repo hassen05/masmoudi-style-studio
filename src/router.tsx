06b889cbacdc7e4a2082db46a544b252b14b0060
hassen05 <hassen.b.a97@gmail.com>
2026-06-23 23:25:52 +0100

    deploy

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,16 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import { createRouter, createHashHistory } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
+  // On crée un historique basé sur le "Hash" (#) adapté à l'hébergement statique GitHub Pages
+  const hashHistory = createHashHistory();
+
   const router = createRouter({
     routeTree,
+    history: hashHistory, // <--- Injection cruciale ici
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
