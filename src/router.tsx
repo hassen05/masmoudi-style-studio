634d3a94204f5b360d26f1f1da4719a485e0795f
hassen05 <hassen.b.a97@gmail.com>
2026-06-24 00:37:27 +0100

    deploy

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
-import { createHashHistory, createMemoryHistory } from "@tanstack/history";
+import { createBrowserHistory , createMemoryHistory } from "@tanstack/history";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
@@ -8,15 +8,15 @@ export const getRouter = () => {
 
   // Use memory history on server (SSR) and hash history in the browser
   const isBrowser = typeof window !== 'undefined' && typeof window.history !== 'undefined' && typeof document !== 'undefined'
-  const history = isBrowser ? createHashHistory() : createMemoryHistory({ initialEntries: ['/'] })
+  const history = isBrowser ? createBrowserHistory () : createMemoryHistory({ initialEntries: ['/'] })
 
-  const router = createRouter({
-    routeTree,
-    history, // <--- Injection cruciale ici
-    context: { queryClient },
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-  });
+ const router = createRouter({
+  routeTree,
+  history,
+  context: { queryClient },
+  basepath: "/masmoudi-style-studio", // 🔥 ADD THIS
+  scrollRestoration: true,
+});
 
   return router;
 };
